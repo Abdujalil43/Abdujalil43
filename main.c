44a9b98ce544019d027b557a28db0f8e1aab7dda
Jalil <abdujalilabdurakhmonov9@gmail.com>
2024-02-27 12:06:38 -0500

    adding function to add two numbers

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
+#define PI 3.14
+
+int add_two(int a, int b) {
+	return a + b;
+}
+
 int main() {
 	printf("Hello, World!\n");
+	int a, b;
+	scanf("%d %d", &a, &b);
+	printf("The sum of two numbers are \n", add_two(a, b));
 	return 0;
 }
